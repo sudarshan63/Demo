7cd7802ee0aff559187e9682cf45eac466708003
sudarshan63 <45875417+sudarshan63@users.noreply.github.com>
2018-12-14 18:20:53 +0530

    Create new 1.php
    Test

---
+++ application/new 1.php
@@ -0,0 +1,6 @@
+1.  You need to install NodeJS. Once you install NodeJS, reload your console or command line and you will have access to NPM.
+		> npm -v    (checking npm version)
+2.	Use NPM to install the Angular CLI.
+		> npm install -g @angular/cli
+3.	Install a new Angular 6 project.
+	> ng new ng6-proj
